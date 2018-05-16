e952f6730cbf8f60534a805c12b471d1d3efa5aa
Javad PourMostafa <javad.pourmostafa@gmail.com>
2018-05-16 11:13:07 +0200

    SA Score system has been implemented

--- InputTxt.py
+++ InputTxt.py
@@ -1,5 +1,5 @@
 text = """What can I say about this place. The staff of the restaurant is 
     nice and the eggplant is not bad. Apart from that, very uninspired food, 
     lack of atmosphere and too expensive. I am a staunch vegetarian and was 
-    sorely dissapointed with the veggie options on the menu. Will be the last 
+    sorely disappointed with the veggie options on the menu. Will be the last 
     time I visit, I recommend others to avoid."""
--- SA.py
+++ SA.py
@@ -1,5 +1,6 @@
 import nltk
 import InputTxt
+import yaml
 
 
 class PreProcess(object):
@@ -26,8 +27,91 @@ class PreProcess(object):
         adjust = [[(word, word, [postag]) for (word, postag) in i] for i in pos]
         return adjust
 
+class DictionaryTagger(object):
+    def __init__(self, dictionaries_path):
+        # Open the path of Dictionary folder
+        files = [open(path, 'r') for path in dictionaries_path]
+        # load each of one dictionary into the solo dictionaries
+        dictionaries = [yaml.load(dictionary_file) for dictionary_file in files]
+        # Close all of the open files
+        map(lambda z: z.close(), files)
+        self.dictionary = {}
+        self.max_key_size = 0
+        for current_dict in dictionaries:
+            for key in current_dict:
+                if key in self.dictionary:
+                    self.dictionary[key].extend(current_dict[key])
+                else:
+                    self.dictionary[key] = current_dict[key]
+                    self.max_key_size = max(self.max_key_size, len(key))
+
+    def tag(self, pos):
+        return [self.tag_sentence(sentence) for sentence in pos]
+
+    def tag_sentence(self, sentence, tag_lemma=False):
+        tag_sentence = []
+        N = len(sentence)
+        if self.max_key_size == 0:
+            self.max_key_size = N
+        i = 0
+        while i < N:
+            j = min(i + self.max_key_size, N)
+            tagged = False
+            while j > i:
+                expression_form = ' '.join([word[0] for word in sentence[i:j]]).lower()
+                expression_lemma = ' '.join([word[1] for word in sentence[i:j]]).lower()
+                if tag_lemma:
+                    literal = expression_lemma
+                else:
+                    literal = expression_form
+                if literal in self.dictionary:
+                    is_single_token = j - i == 1
+                    original_position = i
+                    i = j
+                    taggings = [tag for tag in self.dictionary[literal]]
+                    tagged_expression = (expression_form, expression_lemma, taggings)
+                    if is_single_token:
+                        original_token_tagging = sentence[original_position][2]
+                        tagged_expression[2].extend(original_token_tagging)
+                    tag_sentence.append(tagged_expression)
+                    tagged = True
+                else:
+                    j = j - 1
+            if not tagged:
+                tag_sentence.append(sentence[i])
+                i += 1
+        return tag_sentence
+
+
+class SaScore(object):
+    def __init__(self):
+        pass
+
+    def value_of(self, sentiment):
+        if sentiment == 'positive':
+            return 1
+        if sentiment == 'negative':
+            return -1
+        return 0
+
+    def sa_score(self, dictionary_tagged_sentence):
+        return sum([self.value_of(tag) for sentence in dictionary_tagged_sentence for token in sentence for tag in token[2]])
+
 
 pre = PreProcess()
-tokenized = pre.split(InputTxt.text)
-pos = pre.pos_tagger(tokenized)
-print(pre.adjust(pos))
+SplittedSentences = pre.split(InputTxt.text)
+POS = pre.pos_tagger(SplittedSentences)
+Adjust = pre.adjust(POS)
+DicTagger = DictionaryTagger(['Dictionary/positive.yml', 'Dictionary/negative.yml'])
+dict_tagged_sentences = DicTagger.tag(Adjust)
+SentimentScore = SaScore()
+Score = SentimentScore.sa_score(dict_tagged_sentences)
+
+
+# print(SplittedSentences)
+# print(POS)
+# print(Adjust)
+print(dict_tagged_sentences)
+
+print("..... Sentiment Analysing .....")
+print(Score)
