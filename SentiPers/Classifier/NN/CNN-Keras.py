b3fce1f6de1b4f724f81c18b04fff193bf2622fd
parsa-abbasi <parsa.abbasi1996@gmail.com>
2018-12-30 03:24:14 +0330

    Rename KerasEmbedding model to CNN-Keras

--- SentiPers/Classifier/NN/CNN-Keras.py
+++ SentiPers/Classifier/NN/CNN-Keras.py
@@ -4,13 +4,13 @@ from keras.layers import Flatten
 from keras.layers import Embedding
 from keras.layers.convolutional import Conv1D
 from keras.layers.convolutional import MaxPooling1D
-from SentiPers.WordEmbedding import KerasWE
+from SentiPers.WordEmbedding import PreprocessWE
 from keras.utils.np_utils import to_categorical
 from keras.metrics import categorical_accuracy
 
 # Get data and sizes from Keras Word Embedding
-x_train, x_test, y_train, y_test = KerasWE.get_data()
-vocab_size, max_length = KerasWE.get_sizes()
+x_train, x_test, y_train, y_test = PreprocessWE.get_data()
+vocab_size, max_length = PreprocessWE.get_sizes()
 
 categorical_y_train = to_categorical(y_train, 5)
 categorical_y_test = to_categorical(y_test, 5)
