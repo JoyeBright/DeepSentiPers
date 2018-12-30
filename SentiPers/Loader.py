9f830b8f4492f2f32dfdd2dfca8fa17123d1b768
parsa-abbasi <parsa.abbasi1996@gmail.com>
2018-12-30 03:32:19 +0330

    Remove normalizer (Improve up to 10% in NN models)

--- SentiPers/Loader.py
+++ SentiPers/Loader.py
@@ -1,10 +1,9 @@
 import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from hazm import *
 from SentiPers.Router import ROOT_DIR
 
-n = 7415
+data_size = 7415
 
 
 def ingest():
@@ -15,14 +14,6 @@ def ingest():
     # map applies a function on each cell of Value then if it null, it will set it with 0s
     data['Value'] = data['Value'].map(int)
     data = data[data['Text'].isnull() == False]
-    normalizer = Normalizer()   # Normalizing text using Hazm library (Remove half space)
-    for i in range(0, n):
-        string = normalizer.normalize(data.iloc[i].Text)    # Normalizer
-        # Add some other instances
-        string = string.replace('سیستم عامل', 'سیستم\u200cعامل')
-        string = string.replace('نرم افزار', 'نرم\u200cافزار')
-        string = string.replace('سخت افزار', 'سخت\u200cافزار')
-        data.set_value(i, 'Text', string)
     data.reset_index(inplace=True)
     data.drop('index', axis=1, inplace=True)
     print('Dataset Loaded with shape', data.shape)
@@ -30,8 +21,7 @@ def ingest():
 
 
 ingest = ingest()   # Get all text
-x_train, x_test, y_train, y_test = train_test_split(ingest.head(n).Text,
-                                                    ingest.head(n).Value)
+x_train, x_test, y_train, y_test = train_test_split(ingest.head(data_size).Text, ingest.head(data_size).Value)
 
 
 def get_data():
